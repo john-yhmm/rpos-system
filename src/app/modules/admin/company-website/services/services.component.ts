1437a28d55201b5f6892272641b290e1d8065ee2
HTOO PYAE SHAN <pyaehtoo333@gmail.com>
2025-03-22 00:44:58 +0700

    Added services page and images

--- src/app/modules/admin/company-website/services/services.component.ts
+++ src/app/modules/admin/company-website/services/services.component.ts
@@ -1,9 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './services.component.html',
   encapsulation: ViewEncapsulation.None,})
 export class ServicesComponent {
