90a729f3ddf23ad3e6d4cae4de07db32047b0b89
ThetSweLin <thetswelin74@gmail.com>
2025-03-21 12:22:53 +0700

    Added ui and images

--- src/app/modules/admin/company-website/about-us/about-us.component.ts
+++ src/app/modules/admin/company-website/about-us/about-us.component.ts
@@ -1,17 +1,42 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-    selector     : 'about-us',
+    selector     : "about-us",
     standalone   : true,
-    templateUrl  : './about-us.component.html',
+    imports      : [CommonModule],
+    templateUrl  : "./about-us.component.html",
     encapsulation: ViewEncapsulation.None,
 })
 export class AboutUsComponent
 {
-    /**
-     * Constructor
-     */
-    constructor()
-    {
+    imagePath = "/public/images/pages/company-website/about-us/member.png";
+
+    allMembers = [
+        { name: "John Ye Htet Maung Maung", position: "Developer", image: this.imagePath },
+        { name: "Khin Kyawt Shinn", position: "Developer", image:this.imagePath },
+        { name: "Htoo Pyae Shan", position: "Developer", image:this.imagePath },
+        { name: "Sai Sithu Phyo", position: "Developer", image:this.imagePath },
+        { name: "Saw Thomas Maung Maung Kyaw", position: "Developer", image:this.imagePath },
+        { name: "Min Swan Pyae", position: "Developer", image:this.imagePath },
+        { name: "Naw Hkoo Ku Htoo", position: "Developer", image:this.imagePath },
+        { name: "Chann Myae Zaw", position: "Developer", image:this.imagePath },
+        { name: "Thet Swe Lin", position: "Developer", image:this.imagePath },
+        { name: "John Doe", position: "Designer", image:this.imagePath },
+        { name: "Alexa", position: "Designer", image:this.imagePath }
+    ].sort((a, b) => a.name.localeCompare(b.name));
+
+    members = this.allMembers;
+    selectedRole: string = 'All Members';
+
+    constructor() {}
+
+    filterMembers(role: string): void {
+        this.selectedRole = role;
+        if (role === 'All Members') {
+            this.members = this.allMembers;
+        } else {
+            this.members = this.allMembers.filter(member => member.position === role);
+        }
     }
 }
